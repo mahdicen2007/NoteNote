80fcd4343ae6ac6f874b8bf4fe71bab614067511
Mahdi <mahdicencen@gmail.com>
2024-11-08 21:15:52 +0330

    All resources added

--- app/src/main/java/com/example/teamgit/ui/MainActivity.kt
+++ app/src/main/java/com/example/teamgit/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.teamgit
+package com.example.teamgit.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
---
+++ app/src/main/java/com/example/teamgit/ui/features/HomeScreenUi.kt
@@ -0,0 +1,7 @@
+package com.example.teamgit.ui.features
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreenUi() {
+}
