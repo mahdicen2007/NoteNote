fa69b77fc9343ac1febbb0da0ef31815381d26a1
Mahdi <mahdicencen@gmail.com>
2024-11-08 21:55:22 +0330

    Koin Di prepeared

--- app/src/main/java/com/example/teamgit/ui/MainActivity.kt
+++ app/src/main/java/com/example/teamgit/ui/MainActivity.kt
@@ -1,23 +1,42 @@
 package com.example.teamgit.ui
 
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.LayoutDirection
+import com.example.teamgit.di.myModules
 import com.example.teamgit.ui.theme.TeamGitTheme
+import dev.burnoo.cokoin.Koin
+import org.koin.android.ext.koin.androidContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        window.decorView.layoutDirection = View.LAYOUT_DIRECTION_RTL
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
         setContent {
-            TeamGitTheme {
+            CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
+                Koin(
+                    appDeclaration = {
+                        androidContext(this@MainActivity)
+                        modules(myModules)
+                    }
+                ) {
+                    TeamGitTheme {
 
+                    }
+                }
             }
         }
     }
