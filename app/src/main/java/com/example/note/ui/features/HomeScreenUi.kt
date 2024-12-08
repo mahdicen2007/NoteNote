a6d766365ee422aeedc0ee3d7cf93afb9457ac1d
Mahdi <mahdicencen@gmail.com>
2024-12-08 12:39:38 +0330

    Toolbar prepread

--- app/src/main/java/com/example/note/ui/features/HomeScreenUi.kt
+++ app/src/main/java/com/example/note/ui/features/HomeScreenUi.kt
@@ -5,7 +5,9 @@ import android.app.Activity
 import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Scaffold
 import androidx.compose.material.rememberScaffoldState
@@ -16,9 +18,11 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.note.ui.theme.cBackground
 import com.example.note.ui.widgets.HomeContent
+import com.example.note.ui.widgets.HomeToolbar
 import dev.burnoo.cokoin.navigation.getNavController
 import dev.burnoo.cokoin.viewmodel.getViewModel
 import kotlin.math.log
@@ -43,7 +47,12 @@ fun HomeScreenUi() {
 
     Scaffold(
         scaffoldState = scaffoldState,
-        topBar = {},
+        topBar = {
+            HomeToolbar(
+                onSearchClicked = {},
+                onDrawerCLicked = {}
+            )
+        },
         modifier = Modifier.fillMaxSize(),
         drawerGesturesEnabled = true, // to open drawer by finger without tapping any button,
         drawerContent = {
---
+++ app/src/main/java/com/example/note/ui/widgets/AppWidgets.kt
@@ -0,0 +1,52 @@
+package com.example.note.ui.widgets
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.note.ui.theme.cBorder
+import com.example.note.ui.theme.cPrimary
+import com.example.note.ui.theme.radius2
+
+@Composable
+fun MainButton(
+    modifier: Modifier,
+    src: Int,
+    onButtonCLick: () -> Unit
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+
+    Box(
+        modifier = modifier.size(40.dp)
+            .border(BorderStroke((1.7).dp , SolidColor(cBorder)) , radius2)
+            .padding(0.dp)
+    ) {
+        Icon(
+            painter = painterResource(id = src),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(4.dp)
+                .size(24.dp)
+                .align(Alignment.Center)
+                .clickable(
+                    interactionSource = interactionSource,
+                    indication = null
+                ) {
+                    onButtonCLick.invoke()
+                }
+        )
+    }
+
+}
